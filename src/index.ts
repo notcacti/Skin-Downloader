0d5823d02183ec8869e89189ac7169d71c02d2fd
cacti <127884292+notcacti@users.noreply.github.com>
2025-02-02 15:24:53 +0530

    feat(electron): make a valid electron app && fix UI bugs.

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { app, BrowserWindow, Menu } from "electron";
+import path from "path";
+
+let mainWindow: BrowserWindow | null;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 845,
+        height: 425,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            devTools: true,
+        },
+    });
+
+    Menu.setApplicationMenu(null);
+
+    mainWindow
+        .loadFile(path.join(process.cwd(), "wwwroot", "index.html"))
+        .catch((err) => {
+            console.error("Failed to load the application: ", err);
+        });
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
